8adbf465e7543803577b58b26a56c26bee9d6d44
leomatt547 <leo.matt.547@gmail.com>
2021-10-28 11:13:56 +0700

    fix file

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ app.secret_key = os.urandom(24)
 def home():
     return render_template("index.html")
 
+#Paillier
+@app.route('/paillier/enkripsi')
+def paillier_enkripsi():
+    return render_template("paillier_enkripsi.html")
+
+@app.route('/paillier/dekripsi')
+def paillier_dekripsi():
+    return render_template("paillier_dekripsi.html")
+
+@app.route('/paillier/genKey')
+def paillier_genKey():
+    return render_template("paillier_key.html")
+
+#ECC
 @app.route('/ecc/enkripsi')
 def ecc_enkripsi():
     return render_template("ecc_enkripsi.html")
